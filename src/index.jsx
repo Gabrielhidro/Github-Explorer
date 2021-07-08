78d2da33f76c8fa7007d9c3a66a618ff8bb97cbb
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-08 16:49:13 -0300

    Add devserver, sourcemap and variables dev-prod

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,4 @@
-import React from "react";
+import { render } from "react-dom";
 import { App } from "./App";
+
+render(<App />, document.getElementById("root")); //No método render se usam 2 argumentos, primeiro é o elemento a ser renderizado (html) e o segundo é onde ele será inserido.
