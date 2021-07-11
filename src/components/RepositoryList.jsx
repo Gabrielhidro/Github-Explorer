cb01ac474bd018aa49d8a365e5d3618d10df1fd6
GabrielHidro <gabrielhidro.dev@gmail.com>
2021-07-11 20:14:41 -0300

    listing repositories datas from API  with useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,24 @@
 import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
 import "../styles/repositories.scss";
 
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/",
-};
-
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/gabrielHidro/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return <RepositoryItem key={repository.id} repository={repository} />;
+        })}
       </ul>
     </section>
   );
